7f2b2682535adea5346328abb5e6c80f3a28a3c1
Benoit Vasseur <bvasseur@sii.fr>
2017-02-22 10:12:11 +0100

    change hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Dej-tech!')
 })
 
 app.listen(3000, function () {
